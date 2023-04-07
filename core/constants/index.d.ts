7fd8faf4d23197375a619dcc99286a8c83dd0ced
govard-dev <volodymyr.k@lineir.me>
2023-04-07 13:53:39 +0300

    ADD: BN constants

--- core/constants/index.d.ts
+++ core/constants/index.d.ts
@@ -1,3 +1,5 @@
 export * from './addresses';
 export * from './tokens';
 export declare const INIT_CODE_HASH: string;
+export declare const ZERO_BN: import("bignumber.js").BigNumber;
+export declare const ONE_BN: import("bignumber.js").BigNumber;
