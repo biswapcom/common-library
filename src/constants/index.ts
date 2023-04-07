829ddf4fce6da6e86fb4ddcb4a27293438e8feaf
govard-dev <volodymyr.k@lineir.me>
2023-04-07 16:17:13 +0300

    ADD: BN constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,4 +6,4 @@ export * from './tokens';
 export const INIT_CODE_HASH: string = '0xfea293c909d87cd4153593f077b76bb7e94340200f4ee84211ae8e4f9bd7ffdf';
 
 export const ZERO_BN = toBN( 0);
-export const ONE_BN =toBN(1e18.toString());
+export const ONE_BN = toBN(1e18.toString());
